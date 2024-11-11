011a6763f1418c2d381e65eb063d591f4bce6da1
syam1310 <psyam0175@gmail.com>
2024-11-11 12:30:14 +0530

    introducing spring profile

---
+++ ms-section6/v1-springboot/accounts/src/test/java/com/bank_cards/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank_cards.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ms-section6/v2-spring-cloud-config/accounts/src/main/java/com/bank_cards/AccountsApplication.java
@@ -0,0 +1,46 @@
+package com.bank_cards;
+
+import com.bank_cards.accounts.dto.AccountsContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.eazybytes.accounts.controller") })
+@EnableJpaRepositories("com.eazybytes.accounts.repository")
+@EntityScan("com.eazybytes.accounts.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = AccountsContactInfoDto.class)
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API Documentation",
+                description = "Bank_MS Accounts microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Shyam Patel",
+                        email = "tutor@mybank.com",
+                        url = "https://www.mybank.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.mybank.com"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "MyBank Accounts microservice REST API Documentation",
+                url = "https://www.mybank.com/swagger-ui.html"
+        )
+)
+public class AccountsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsApplication.class, args);
+    }
+
+}
